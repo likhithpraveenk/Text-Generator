f4d02c07e04326d0c7cad8a9c0805aa5ac5d90de
Likhith Praveen <lpkatlagunta@gmail.com>
2022-01-05 12:23:05 +0530

    generating sentences based on trigrams

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -5,34 +5,47 @@ from random import choice, choices
 
 def make_dictionary(arr: list) -> dict:
     heads = defaultdict(dict)
-    for h, t in arr:
+    for h1, h2, t in arr:
+        h = (h1, h2)
         heads[h][t] = heads[h].get(t, 0) + 1
     return heads
 
 
 def generate_pseudo_sentence(dic: dict) -> list:
-    word = get_first_word(dic)
-    res = [word]
+    h1, h2 = get_first_word(dic)
+    res = [h1, h2]
+    i = 0
     while True:
-        keys = list(dic[word].keys())
-        weights = list(dic[word].values())
-        [word] = choices(keys, weights)
+        if i >= 20:
+            return generate_pseudo_sentence(dic)
+        word = get_tail(dic, h1, h2)
         if len(res) >= 5 and is_last_word(word):
             res.append(word)
             break
-        elif len(res) < 5 and is_last_word(word):
+        elif is_last_word(word):
+            i += 1
             continue
         else:
             res.append(word)
+            h1, h2 = res[-2], res[-1]
     return res
 
 
-def get_first_word(dic: dict) -> str:
+def get_first_word(dic: dict) -> tuple[str, str]:
     arr = list(dic.keys())
-    word = choice(arr)
-    while not word[0].isupper() or is_last_word(word):
-        word = choice(arr)
+    h1, h2 = choice(arr)
+    while not h1[0].isupper() or is_last_word(h1):
+        h1, h2 = choice(arr)
     arr.clear()
+    return h1, h2
+
+
+def get_tail(dic: dict, h1: str, h2: str) -> str:
+    keys = list(dic[(h1, h2)].keys())
+    weights = list(dic[(h1, h2)].values())
+    [word] = choices(keys, weights)
+    keys.clear()
+    weights.clear()
     return word
 
 
@@ -43,8 +56,8 @@ def is_last_word(word: str) -> bool:
 if __name__ == "__main__":
     filename = input()
     with open(filename, "r", encoding="utf-8") as f:
-        bigrams = list(ngrams(f.read().split(), 2))
-        dictionary = make_dictionary(bigrams)
+        trigrams = list(ngrams(f.read().split(), 3))
+        dictionary = make_dictionary(trigrams)
         for _ in range(10):
             sentence = generate_pseudo_sentence(dictionary)
             print(" ".join(sentence))
