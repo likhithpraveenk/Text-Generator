041d1739d74e4ddf9c4f1d3ee5bdbd98fc6ce826
Likhith Praveen <lpkatlagunta@gmail.com>
2022-01-05 09:45:24 +0530

    bigrams constructed

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,23 +1,20 @@
-# from nltk import word_tokenize
-from collections import Counter
+from nltk.util import ngrams
 
 
 if __name__ == "__main__":
     filename = input()
     with open(filename, "r", encoding="utf-8") as f:
-        # tokens = word_tokenize(f.read())
         tokens = f.read().split()
-        print("Corpus statistics")
-        print(f"All tokens: {len(tokens)}")
-        print(f"Unique tokens: {len(Counter(tokens))}")
+        bigrams = list(ngrams(tokens, 2))
+        print(f"Number of bigrams: {len(bigrams)}")
         while True:
             ind = input()
             if ind == "exit":
                 break
             try:
                 ind = int(ind)
-                print(tokens[ind])
+                print(f"Head: {bigrams[ind][0]} Tail: {bigrams[ind][1]}")
             except (TypeError, ValueError):
                 print("Type Error. Please input an integer.")
             except IndexError:
-                print("Index Error. Please input an integer that is in the range of the corpus.")
+                print("Index Error. Please input a value that is not greater than the number of all bigrams.")
