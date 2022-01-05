59e1d988a91fafbbf224b8b02a2b73685f2cb960
Likhith Praveen <lpkatlagunta@gmail.com>
2022-01-05 10:10:46 +0530

    simplified markov chain

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,19 +1,31 @@
 from nltk.util import ngrams
+from collections import defaultdict
+
+
+def make_dictionary(arr):
+    heads = defaultdict(dict)
+    for h, t in arr:
+        heads[h][t] = heads[h].get(t, 0) + 1
+
+    return heads
 
 
 if __name__ == "__main__":
     filename = input()
     with open(filename, "r", encoding="utf-8") as f:
-        tokens = f.read().split()
-        bigrams = list(ngrams(tokens, 2))
-        print(f"Number of bigrams: {len(bigrams)}")
+        bigrams = list(ngrams(f.read().split(), 2))
+        dictionary = make_dictionary(bigrams)
         while True:
-            ind = input()
-            if ind == "exit":
+            head = input()
+            if head == "exit":
                 break
             try:
-                ind = int(ind)
-                print(f"Head: {bigrams[ind][0]} Tail: {bigrams[ind][1]}")
+                tails = dictionary[head]
+                print(f"Head: {head}")
+                for tail, val in tails.items():
+                    print(f"Tail: {tail}\tCount: {val}")
+            except KeyError:
+                print("Key Error. The requested word is not in the model. Please input another word.")
             except (TypeError, ValueError):
                 print("Type Error. Please input an integer.")
             except IndexError:
